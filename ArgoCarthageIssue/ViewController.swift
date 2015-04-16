76d8b502f56201e7aa7f1e4bc897f16d6d020e5b
Paul Young <paulyoungonline@gmail.com>
2015-04-16 11:27:23 -0700

    Initial Commit

---
+++ ArgoCarthageIssue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ArgoCarthageIssue
+//
+//  Created by Paul Young on 4/16/15.
+//  Copyright (c) 2015 Paul Young. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
